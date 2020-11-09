16aed0607cf9f32ab6e8f12136173fb3d793ea3c
hanssenstijn <hanssenstijn@gmail.com>
2020-11-09 13:14:06 +0100

    create class with mutiple constructors and only getters

--- OOP/src/com/company/VipCustomer.java
+++ OOP/src/com/company/VipCustomer.java
@@ -1,4 +1,35 @@
 package com.company;
 
 public class VipCustomer {
+    private String name;
+    private double creditLimit;
+    private String emailAdress;
+
+    public VipCustomer() {
+        this("default",5000.0,"default");
+    }
+
+    public VipCustomer(String name, double creditLimit) {
+        this(name,creditLimit,"unknown@gmail.com");
+        this.name = name;
+        this.creditLimit = creditLimit;
+    }
+
+    public VipCustomer(String name, double creditLimit, String emailAdress) {
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.emailAdress = emailAdress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getEmailAdress() {
+        return emailAdress;
+    }
 }
