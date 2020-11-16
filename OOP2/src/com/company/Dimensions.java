634a1c481fed3306fe901cc962f52491e3e107ff
hanssenstijn <hanssenstijn@gmail.com>
2020-11-16 13:50:04 +0100

    Set up PC class

---
+++ OOP2/src/com/company/Dimensions.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Dimensions {
+    private int width;
+    private int hiefht;
+    private  int depth;
+
+    public Dimensions(int width, int hiefht, int depth) {
+        this.width = width;
+        this.hiefht = hiefht;
+        this.depth = depth;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHiefht() {
+        return hiefht;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+}
+
